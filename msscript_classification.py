b065a9604b6cac1aefc73afbbe95b9a70df17914
Ankit Seth <cosmic.ankit@gmail.com>
2021-06-14 12:51:27 +0000

    Update msscript_classification.py

--- msscript_classification.py
+++ msscript_classification.py
@@ -43,18 +43,22 @@ while True:
     if message and not message['data'] == 1:
         cache_value = redis_client.hgetall(message["data"])
         if str(cache_value[b'step'].decode("utf-8"))=="2":
-            import detectron as det
-            import classification as clsf
-            image_url = cache_value[b'image_url'].decode("utf-8")
-            print(image_url)
-            image_batch,image_details = det.crop_images(image_url)
-            imageclassifierdetailslist = clsf.result(image_batch)
-            #imageclassifierdetailslist=[]
-            print("ml image processed")
-            pid=message["data"].decode("utf-8")
-            val=col.update_one({"_id":pid},{"$set":{"ml_classified_info":imageclassifierdetailslist,"check":True,"ml_detectron_info":image_details}})
-            del_data= redis_client.delete(message["data"])
-            print(del_data)
+            try:
+                import detectron as det
+                import classification as clsf
+                image_url = cache_value[b'image_url'].decode("utf-8")
+                print(image_url)
+                image_batch,image_details = det.crop_images(image_url)
+                imageclassifierdetailslist = clsf.result(image_batch)
+                #imageclassifierdetailslist=[]
+                print("ml image processed")
+                pid=message["data"].decode("utf-8")
+                val=col.update_one({"_id":pid},{"$set":{"ml_classified_info":imageclassifierdetailslist,"check":True,"ml_detectron_info":image_details}})
+                del_data= redis_client.delete(message["data"])
+                print(del_data)
+            except:
+                print('error')  
+                del_data= redis_client.delete(message["data"])  
             #os.execv(sys.executable, ['python3'] + sys.argv)
                 # do something with the message
     time.sleep(0.001)  # be nice to the system :)
